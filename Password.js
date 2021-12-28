7b3d09b365f9c91258567c6164092670cd815d4a
CYOuO <74392547+CYOuO@users.noreply.github.com>
2021-12-29 07:34:51 +0800

    Add files via upload

--- Password.js
+++ Password.js
@@ -1,20 +1,20 @@
 function password(pwd) {
   event.preventDefault();
   if (pwd == "玉山") {
-    alert("你真厲害");
+    alert("新高山就是玉山！");
     window.location.href = "./Scripts/000.html";
   } else {
-    alert("Denied !");
+    alert("記得不是這個！");
   }
 }
 
 function password1(pwd) {
   event.preventDefault();
   if (pwd == "海老茶色") {
-    alert("對！我想起來了！");
+    alert("對！想起來了！");
     window.location.href = "./Scripts/000.html";
   } else {
-    alert("我記得不是這個！");
+    alert("記得不是這個！");
   }
 }
 
@@ -40,6 +40,16 @@ function password4(pwd) {
   event.preventDefault();
   if (pwd == "74206581") {
     alert("你真厲害");
+    window.location.href = "./RingBell.html";
+  } else {
+    alert("Denied !");
+  }
+}
+
+function password5(pwd) {
+  event.preventDefault();
+  if (pwd == "浪漫") {
+    alert("沒錯，正是這種浪漫的氛圍渲染了整個大正時代");
     window.location.href = "./RingBell.html";
   } else {
     alert("Denied !");
