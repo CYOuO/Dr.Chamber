f412f59fbbf5362f31bc0638851f3e1c91ea950e
CYOuO <joy1001234@gmail.com>
2021-12-29 00:47:35 +0800

    test

--- YuShan.js
+++ YuShan.js
@@ -2,7 +2,7 @@ function password(pwd) {
   event.preventDefault();
   if (pwd == "玉山") {
     alert("你真厲害");
-    window.location.href = "./RingBell.html";
+    window.location.href = "./Scripts/000.html";
   } else {
     alert("Denied !");
   }
