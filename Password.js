cb12a14cf9b15e92eafa53452600e5ee3681a5d9
CYOuO <74392547+CYOuO@users.noreply.github.com>
2021-12-29 14:17:28 +0800

    Update Password.js

--- Password.js
+++ Password.js
@@ -40,7 +40,7 @@ function password3(pwd) {
 
 function password4(pwd) {
   event.preventDefault();
-  if (pwd == "") {
+  if (pwd == "1480") {
     alert("解開鎖了");
     window.location.href = "./Scripts/002.html";
   } else {
