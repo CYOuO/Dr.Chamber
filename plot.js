43ccd0f9d4fe66bddb938ed791e59c58d7565c29
CYOuO <74392547+CYOuO@users.noreply.github.com>
2021-12-29 14:31:49 +0800

    Update plot.js

--- plot.js
+++ plot.js
@@ -19,8 +19,65 @@ function next0() {
     document.getElementById("p5").style.display = "none";
     document.getElementById("p6").style.display = "block";
   });
+  document.getElementById("next6").addEventListener("click", function () {
+    document.getElementById("p6").style.display = "none";
+    document.getElementById("p7").style.display = "block";
+  });
+  document.getElementById("next7").addEventListener("click", function () {
+    document.getElementById("p7").style.display = "none";
+    document.getElementById("p8").style.display = "block";
+  });
+  document.getElementById("next8").addEventListener("click", function () {
+    document.getElementById("p8").style.display = "none";
+    document.getElementById("p9").style.display = "block";
+  });
+  document.getElementById("next9").addEventListener("click", function () {
+    document.getElementById("p9").style.display = "none";
+    document.getElementById("p10").style.display = "block";
+  });
+  document.getElementById("next10").addEventListener("click", function () {
+    document.getElementById("p10").style.display = "none";
+    document.getElementById("p11").style.display = "block";
+  });
+  document.getElementById("next11").addEventListener("click", function () {
+    document.getElementById("p11").style.display = "none";
+    document.getElementById("p12").style.display = "block";
+  });
+  document.getElementById("next12").addEventListener("click", function () {
+    document.getElementById("p12").style.display = "none";
+    document.getElementById("p13").style.display = "block";
+  });
+  document.getElementById("next13").addEventListener("click", function () {
+    document.getElementById("p13").style.display = "none";
+    document.getElementById("p14").style.display = "block";
+  });  document.getElementById("next14").addEventListener("click", function () {
+    document.getElementById("p14").style.display = "none";
+    document.getElementById("p15").style.display = "block";
+  });
+  document.getElementById("next15").addEventListener("click", function () {
+    document.getElementById("p15").style.display = "none";
+    document.getElementById("p16").style.display = "block";
+  });
+  document.getElementById("next16").addEventListener("click", function () {
+    document.getElementById("p16").style.display = "none";
+    document.getElementById("p17").style.display = "block";
+  });
+  document.getElementById("next17").addEventListener("click", function () {
+    document.getElementById("p17").style.display = "none";
+    document.getElementById("p18").style.display = "block";
+  });
+  document.getElementById("next18").addEventListener("click", function () {
+    document.getElementById("p18").style.display = "none";
+    document.getElementById("p19").style.display = "block";
+  });
+  document.getElementById("next19").addEventListener("click", function () {
+    document.getElementById("p19").style.display = "none";
+    document.getElementById("p20").style.display = "block";
+  });
+
 }
 
 window.addEventListener("load", function () {
   next0();
 });
+
